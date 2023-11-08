87d9210d598d1d1672742c4f9515bf5a8d86d903
Bot-code-2003 <135417049+Bot-code-2003@users.noreply.github.com>
2023-11-08 14:47:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.querySelector(".img1").setAttribute("src", randomImageSrc);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage = "dice" + randomNumber2 + ".png";
-var randomImageSrc = "./images/" + randomDiceImage;
+var randomImageSrc = "./" + randomDiceImage;
 document.querySelector(".img2").setAttribute("src", randomImageSrc);
 
 if (randomNumber1 > randomNumber2) {
