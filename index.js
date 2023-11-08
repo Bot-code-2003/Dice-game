cb264dc3ed98726839c0b003527fb76f6baa9d1a
Bot-code-2003 <135417049+Bot-code-2003@users.noreply.github.com>
2023-11-08 14:08:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// alert("Hello Master!");
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSrc = "./images/" + randomDiceImage;
+document.querySelector(".img1").setAttribute("src", randomImageSrc);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber2 + ".png";
+var randomImageSrc = "./images/" + randomDiceImage;
+document.querySelector(".img2").setAttribute("src", randomImageSrc);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🎉Player1 wins🎉";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🎉Player2 wins🎉";
+} else {
+  document.querySelector("h1").innerHTML = "Its a Draw!";
+  document.querySelector("body").style.backgroundColor = "tomato";
+}
