e8c80bebfeee6345745bd66e91749665ad4a8097
Bot-code-2003 <135417049+Bot-code-2003@users.noreply.github.com>
2023-11-08 14:22:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // alert("Hello Master!");
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randomImageSrc = "./images/" + randomDiceImage;
+var randomImageSrc = "./" + randomDiceImage;
 document.querySelector(".img1").setAttribute("src", randomImageSrc);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
